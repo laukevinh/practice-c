1be52b7f3dd14667aba5d2cc0155fab0cd70c44f
klau <laudevelopment@gmail.com>
2021-05-23 05:11:28 -0500

    Add char list

--- list.c
+++ list.c
@@ -1,197 +1,139 @@
 #include <stdlib.h>
 #include <assert.h>
+#include <stdio.h>
 
-#define SIZE 8
+#define CAPACITY 8
 #define GROW 2
 #define SHRINK 4
 
 struct List {
-    int *_A;
     int _capacity;
-    int _n;
+    int _size;
+    char **_A;
 };
 
-struct List *newlist(void);
+struct List *new_list(void);
 int len(struct List *);
-int at(struct List *, int);
-struct List *append(struct List *, int);
-struct List *resize(struct List *, int);
-int pop(struct List *);
-struct List *insert(struct List *, int, int);
-int pop_at(struct List *, int);
-int remove2(struct List *, int);
-int find(struct List *, int);
-struct List *remove_all(struct List *, int);
+char *at(struct List *, int);
+char *set(struct List *, char *, int);
+struct List *append(struct List *, char *);
+// struct List *resize(struct List *, int);
+// int pop(struct List *);
+// struct List *insert(struct List *, int, int);
+// int pop_at(struct List *, int);
+// int remove2(struct List *, int);
+// int find(struct List *, int);
+// struct List *remove_all(struct List *, int);
+int strcmp(char *, char*);
 
 int main(void)
 {
-    struct List *list;
-    
-    list = newlist();
+    struct List *L = new_list();
+
+    // test len
+    assert(len(L) == 0);
+    // test set
+    assert(set(L, "Hello", 0) == NULL);
+    // test append
+    append(L, "Hello");
+    assert(strcmp(at(L, 0), "Hello"));
+    assert(len(L) == 1);
+    // test resize
+
     for(int i = 0; i < 16; i++)
-        append(list, i);
-    assert(at(list, 14) == 14);
-    assert(len(list) == 16);
-    assert(pop(list) == 15);
-    assert(len(list) == 15);
-    insert(list, 0, 1);
-    insert(list, 0, 2);
-    assert(len(list) == 17);
-    assert(at(list, len(list) - 1) == 14);
-    assert(pop_at(list, 0) == 2);
-    assert(pop_at(list, len(list) - 1) == 14);
-    assert(len(list) == 15);
-    assert(find(list, 14) == -1);
-    assert(remove2(list, 14) == -1);
-    assert(find(list, 13) == 14);
-    assert(remove2(list, 13) == 13);
-    assert(len(list) == 14);
-    remove_all(list, 1);
-    assert(len(list) == 12);
+        append(L, "Hello");
+    append(L, "World");
+    // for (int i=0; i<len(L); i++)
+    //     printf("%3d %s\n", i, at(L, i));
+    assert(strcmp(at(L, len(L) - 1), "World"));
+    // assert(at(list, 14) == 14);
+    // assert(len(list) == 16);
+    // assert(pop(list) == 15);
+    // assert(len(list) == 15);
+    // insert(list, 0, 1);
+    // insert(list, 0, 2);
+    // assert(len(list) == 17);
+    // assert(at(list, len(list) - 1) == 14);
+    // assert(pop_at(list, 0) == 2);
+    // assert(pop_at(list, len(list) - 1) == 14);
+    // assert(len(list) == 15);
+    // assert(find(list, 14) == -1);
+    // assert(remove2(list, 14) == -1);
+    // assert(find(list, 13) == 14);
+    // assert(remove2(list, 13) == 13);
+    // assert(len(list) == 14);
+    // remove_all(list, 1);
+    // assert(len(list) == 12);
     return 0;
 }
 
-struct List *newlist(void) 
-{
-    struct List *L = (struct List *) malloc(sizeof (struct List));
-    L->_A = (int *) malloc(sizeof (int) * SIZE);
-    L->_capacity = SIZE;
-    L->_n = 0;
+struct List *new_list(void) {
+    struct List *L = (struct List *) malloc(sizeof(struct List));
+    L->_capacity = CAPACITY;
+    L->_size = 0;
+    L->_A = (char **) malloc(sizeof(char *) * L->_capacity);
     return L;
 }
 
-int in_bounds(int idx, int n) 
-{
-    return idx >= 0 && idx < n;
+int len(struct List *L) {
+    return L->_size;
 }
 
-int len(struct List *list)
-{
-    return list->_n;
-}
-
-int at(struct List *L, int idx)
-{
-    if (!in_bounds(idx, L->_n))
+int position(struct List *L, int i) {
+    int pos = i;
+    if (pos < 0) {
+        pos += len(L);
+    }
+    if (pos < 0 || len(L) <= pos) {
         return -1;
-    return *(L->_A + idx);
+    } else {
+        return pos;
+    }
 }
 
-struct List *append(struct List *L, int val)
-{
-    if (len(L) >= L->_capacity)
-        resize(L, GROW * L->_capacity);
-    L->_A[L->_n] = val;
-    L->_n++;
-    return L;
-}
-
-struct List *resize(struct List *L, int capacity)
-{
-    int *A = (int *) malloc(sizeof(int) * capacity);
-    for (int i = 0; i < len(L); i++)
-        A[i] = at(L, i);
-    free(L->_A);
-    L->_A = A;
-    L->_capacity = capacity;
-    return L;
-}
-
-int pop(struct List *L)
-{
-    int val;
-
-    if (len(L) < 1)
-        return -1;
-    val = at(L, len(L) - 1);
-    L->_n--;
-    if (len(L) < L->_capacity / SHRINK)
-        resize(L, L->_capacity / GROW);
-    return val;
-}
-
-struct List *insert(struct List *L, int idx, int val)
-{
-    if (!in_bounds(idx, len(L)))
+char *at(struct List *L, int i) {
+    int pos = position(L, i);
+    if (pos < 0) {
         return NULL;
-    if (len(L) + 1 > L->_capacity) {
-        int *A, i;
-        
-        A = (int *) malloc(sizeof(int) * L->_capacity * GROW);
-        for (i=0; i < idx; i++)
-            A[i] = at(L, i);
-        A[i] = val;
-        for ( ; i < len(L); i++)
-            A[i+1] = at(L, i);
-        free(L->_A);
-        L->_A = A;
-        L->_n++;
-        L->_capacity = L->_capacity * GROW;
     } else {
-        int i;
-        
-        for (i = len(L); i > idx; i--)
-            L->_A[i] = L->_A[i-1];
-        L->_A[i] = val;
-        L->_n++;
+        return L->_A[pos];
     }
+}
+
+char *set(struct List *L, char *new, int i) {
+    int pos = position(L, i);
+    if (pos < 0) {
+        return NULL;
+    } else {
+        L->_A[pos] = new;
+        return L->_A[pos];
+    }
+}
+
+char **resize(struct List *L, int size) {
+    char **A = (char **) malloc(sizeof(char *) * size);
+    for (int i=0; i<len(L); i++) {
+        A[i] = L->_A[i];
+    }
+    return A;
+}
+
+struct List *append(struct List *L, char * new) {
+    if (L->_capacity <= L->_size) {
+        char **old = L->_A;
+        L->_A = resize(L, L->_capacity * GROW);
+        L->_capacity *= GROW;
+        free(old);
+    }
+    L->_size++;
+    set(L, new, len(L) - 1);
     return L;
 }
 
-int pop_at(struct List *L, int idx)
-{
-    int val;
-
-    if (!in_bounds(idx, len(L)))
-        return -1;
-    val = at(L, idx);
-    if (len(L) - 1 < L->_capacity / SHRINK) {
-        int *A, i;
-
-        A = (int *) malloc(sizeof(int) * L->_capacity / GROW);
-        for (i = 0; i < idx; i++)
-            A[i] = at(L, i);
-        for ( ; i < len(L) - 1; i++)
-            A[i] = at(L, i+1);
-        free(L->_A);
-        L->_A = A;
-        L->_n--;
-        L->_capacity = L->_capacity / GROW;
-    } else {
-        for (int i = idx; i < len(L) - 1; i++)
-            L->_A[i] = L->_A[i+1];
-        L->_n--;
+int strcmp(char *s, char *t) {
+    for ( ; *s != '\0' && *t != '\0'; *s++, *t++) {
+        if (*s != *t)
+            return 0;
     }
-    return val;
-}
-
-int remove2(struct List *L, int val)
-{
-    int idx;
-
-    if ((idx = find(L, val)) == -1)
-        return -1;
-    return pop_at(L, idx);
-}
-
-int find(struct List *L, int val)
-{
-    for (int i = 0; i < len(L); i++)
-        if (at(L, i) == val)
-            return i;
-    return -1;
-}
-
-struct List *remove_all(struct List *L, int val)
-{
-    int i, j, *A;
-
-    A = (int *) malloc(sizeof(int) * L->_capacity);
-    for (i=0, j=0; i < len(L); i++)
-        if (at(L, i) != val)
-            A[j++] = at(L, i);
-    free(L->_A);
-    L->_A = A;
-    L->_n = j;
-    return L;
+    return 1;
 }
